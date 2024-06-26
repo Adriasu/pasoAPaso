145e1a6b77e75f0c9ddaf918d09b1286691bf1a2
Adriasu <adriana.suarez@students.gammatech.school>
2024-06-26 23:57:46 +0200

    terminado cards y consolelog

--- main.js
+++ main.js
@@ -1,35 +1,47 @@
 import { datajunkFood } from "./data.js";
 
-const button = document.getElementsByTagName("button")
-const divGreen = document.getElementById("box2")
+const button = document.getElementsByTagName("button");
+const divGreen = document.getElementById("box2");
 
 button[0].addEventListener("click", () => {
-    divGreen.style.visibility === "hidden"
+  divGreen.style.visibility === "hidden"
     ? (divGreen.style.visibility = "visible")
     : (divGreen.style.visibility = "hidden");
-})
+});
 
-const card = document.createElement("section")
-const containImg = document.createElement("div")
-const img = document.createElement("img")
-const textContain = document.createElement("div")
-const textName = document.createElement("h1")
-const textPrice = document.createElement("p")
-const buttonCard = document.createElement("button")
+const cardsContain = document.getElementById("cardsContain");
 
-document.querySelector("body").appendChild(card);
-card.appendChild(containImg)
-containImg.appendChild(img)
-card.appendChild(textContain)
-textContain.appendChild(textName)
-textContain.appendChild(textPrice)
-card.appendChild(buttonCard)
+datajunkFood.forEach((food) => {
+  const card = document.createElement("section");
+  const containImg = document.createElement("div");
+  const img = document.createElement("img");
+  const textContain = document.createElement("div");
+  const textName = document.createElement("h1");
+  const textPrice = document.createElement("p");
+  const buttonCard = document.createElement("button");
 
-containImg.id = "containImg"
-textContain.id = "textContain"
+  cardsContain.appendChild(card);
+  card.appendChild(containImg);
+  containImg.appendChild(img);
+  card.appendChild(textContain);
+  textContain.appendChild(textName);
+  textContain.appendChild(textPrice);
+  card.appendChild(buttonCard);
 
-img.src ="https://azure-images.cluvi.com/images-mini/bTYBpU17RS/w_1200_bTYBpU17RS_perro_loco.jpg"
-textName.innerHTML = "Happy Meal"
-textPrice.innerHTML = "4.7 €"
-buttonCard.innerHTML = "Button"
+  containImg.id = "containImg";
+  textContain.id = "textContain";
 
+  img.src = food.img;
+  textName.innerHTML = food.productName;
+  textPrice.innerHTML = `${food.price} €`;
+  buttonCard.innerHTML = "Button";
+
+  buttonCard.addEventListener("click", () => {
+    const result = {
+        img: food.img,
+        productName: food.productName,
+        price: food.price
+    }
+    console.log(result);
+  })
+});
--- utils.js
+++ utils.js
@@ -8,3 +8,28 @@ function generateUUID() {
     return uuid;
   }
  
+
+
+  // const card = document.createElement("section")
+// const containImg = document.createElement("div")
+// const img = document.createElement("img")
+// const textContain = document.createElement("div")
+// const textName = document.createElement("h1")
+// const textPrice = document.createElement("p")
+// const buttonCard = document.createElement("button")
+
+// document.querySelector("body").appendChild(card);
+// card.appendChild(containImg)
+// containImg.appendChild(img)
+// card.appendChild(textContain)
+// textContain.appendChild(textName)
+// textContain.appendChild(textPrice)
+// card.appendChild(buttonCard)
+
+// containImg.id = "containImg"
+// textContain.id = "textContain"
+
+// img.src ="https://azure-images.cluvi.com/images-mini/bTYBpU17RS/w_1200_bTYBpU17RS_perro_loco.jpg"
+// textName.innerHTML = "Happy Meal"
+// textPrice.innerHTML = "4.7 €"
+// buttonCard.innerHTML = "Button"
