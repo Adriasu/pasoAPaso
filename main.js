d082b0ad0bdf69e7a445623ea00e4e5b11cb385d
Adriasu <adriana.suarez@students.gammatech.school>
2024-06-26 23:12:32 +0200

    estilo aplicado

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ button[0].addEventListener("click", () => {
 })
 
 const card = document.createElement("section")
+const containImg = document.createElement("div")
 const img = document.createElement("img")
 const textContain = document.createElement("div")
 const textName = document.createElement("h1")
@@ -17,11 +18,15 @@ const textPrice = document.createElement("p")
 const buttonCard = document.createElement("button")
 
 document.querySelector("body").appendChild(card);
-card.appendChild(img)
+card.appendChild(containImg)
+containImg.appendChild(img)
 card.appendChild(textContain)
 textContain.appendChild(textName)
 textContain.appendChild(textPrice)
 card.appendChild(buttonCard)
+
+containImg.id = "containImg"
+textContain.id = "textContain"
 
 img.src ="https://azure-images.cluvi.com/images-mini/bTYBpU17RS/w_1200_bTYBpU17RS_perro_loco.jpg"
 textName.innerHTML = "Happy Meal"
