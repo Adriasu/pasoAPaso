ddb0f0327a6839567a77e4a9cbaff76d1592dc31
Adriasu <adriana.suarez@students.gammatech.school>
2024-06-26 21:32:09 +0200

    hecha card

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 import { datajunkFood } from "./data.js";
 
-console.log(datajunkFood);
+const button = document.getElementsByTagName("button")
+const divGreen = document.getElementById("box2")
+
+button[0].addEventListener("click", () => {
+    divGreen.style.visibility === "hidden"
+    ? (divGreen.style.visibility = "visible")
+    : (divGreen.style.visibility = "hidden");
+})
+
+const card = document.createElement("section")
+const img = document.createElement("img")
+const textContain = document.createElement("div")
+const textName = document.createElement("h1")
+const textPrice = document.createElement("p")
+const buttonCard = document.createElement("button")
+
+document.querySelector("body").appendChild(card);
+card.appendChild(img)
+card.appendChild(textContain)
+textContain.appendChild(textName)
+textContain.appendChild(textPrice)
+card.appendChild(buttonCard)
+
+img.src ="https://azure-images.cluvi.com/images-mini/bTYBpU17RS/w_1200_bTYBpU17RS_perro_loco.jpg"
+textName.innerHTML = "Happy Meal"
+textPrice.innerHTML = "4.7 €"
+buttonCard.innerHTML = "Button"
+
