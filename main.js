2d4c3dd2055509d3f4a4ac53ef72b251b8206259
Adriasu <adriana.suarez@students.gammatech.school>
2024-06-28 01:33:52 +0200

    inicio del reduce

--- main.js
+++ main.js
@@ -4,24 +4,26 @@ const button = document.getElementById("btnShowHide");
 const divGreen = document.getElementById("box2");
 
 button.addEventListener("click", () => {
-  divGreen.style.visibility === "visible"
-    ? (divGreen.style.visibility = "hidden" ) 
-    : (divGreen.style.visibility = "visible" )
+  divGreen.style.display === "flex"
+    ? ((divGreen.style.display = "none"), (button.innerHTML = "Mostrar"))
+    : ((divGreen.style.display = "flex"), (button.innerHTML = "Ocultar"));
 });
 
 const cardsContain = document.getElementById("cardsContain");
 const boxConteinSelectedProducts = document.getElementById("box2");
-const divConteinArraySelectedProducts = document.createElement("div")
-const btnClear = document.createElement("button")
+const divConteinArraySelectedProducts = document.getElementById(
+  "divConteinArraySelectedProducts"
+);
+const btnClear = document.getElementById("btnClear");
 const selectedProducts = [];
 
-boxConteinSelectedProducts.appendChild(divConteinArraySelectedProducts)
-boxConteinSelectedProducts.appendChild(btnClear)
+boxConteinSelectedProducts.appendChild(divConteinArraySelectedProducts);
+boxConteinSelectedProducts.appendChild(btnClear);
 
-
-btnClear.innerHTML = "CLEAR"
+btnClear.innerHTML = "CLEAR";
 
 btnClear.addEventListener("click", () => {
+  selectedProducts.splice(0, selectedProducts.length);
   divConteinArraySelectedProducts.innerHTML = "";
 });
 
@@ -52,13 +54,32 @@ datajunkFood.forEach((food) => {
 
   buttonCard.addEventListener("click", () => {
     divConteinArraySelectedProducts.innerHTML = "";
-    const result = {
-      img: food.img,
-      productName: food.productName,
-      price: food.price,
-      id: food.id,
-    };
-    selectedProducts.push(result);
+    const productCount = selectedProducts.reduce((acc, product) => {
+      acc[product.id] = acc[product.id] || {
+        count: 0,
+        img: product.img,
+        productName: product.productName,
+        price: product.price,
+      };
+      acc[product.id].count++
+    }, {});
+
+    console.log(productCount);
+
+    // const result = {
+    //   img: food.img,
+    //   productName: food.productName,
+    //   price: food.price,
+    //   id: food.id,
+    // };
+    selectedProducts.push(productCount);
+
+    // const productCount = selectedProducts.reduce((acc, product) => {
+    //   acc[product.id] = (acc[product.id] || 0) + 1;
+    //   return acc;
+    // }, {});
+
+    // console.log(productCount);
 
     selectedProducts.forEach((newProduct) => {
       const card = document.createElement("section");
@@ -67,7 +88,9 @@ datajunkFood.forEach((food) => {
       const textContain = document.createElement("div");
       const textName = document.createElement("h1");
       const textPrice = document.createElement("p");
+      const containCounterBtn = document.createElement("div");
       const buttonCard = document.createElement("img");
+      const counter = document.createElement("p");
 
       divConteinArraySelectedProducts.appendChild(card);
       card.appendChild(containImg);
@@ -75,22 +98,25 @@ datajunkFood.forEach((food) => {
       card.appendChild(textContain);
       textContain.appendChild(textName);
       textContain.appendChild(textPrice);
-      card.appendChild(buttonCard);
+      card.appendChild(containCounterBtn);
+      containCounterBtn.appendChild(counter);
+      containCounterBtn.appendChild(buttonCard);
 
       card.id = "cardSelected";
       containImg.id = "containImgSelected";
       textContain.id = "textContain";
       buttonCard.id = "btnDelete";
+      containCounterBtn.id = "containCounterBtn";
 
       img.src = newProduct.img;
       textName.innerHTML = newProduct.productName;
       textPrice.innerHTML = `${newProduct.price} €`;
       buttonCard.src =
         "https://img.icons8.com/?size=24&id=BsNkc7jnuBPU&format=png";
+
       buttonCard.addEventListener("click", (event) => {
-        event.target.parentElement.remove();
+        event.target.parentElement.parentElement.remove();
       });
     });
   });
 });
-
